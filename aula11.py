de6ae5db168a7d6847f3888fc683868c01fb147f
Lucas <lucasbezerra06@gmail.com>
2018-05-30 09:44:45 -0300

    aula 11

---
+++ aula11.py
@@ -0,0 +1,5 @@
+print('\33[4;30;45mOlá, Mundo!\33[m')
+print('\33[7;30mOlá Mundo!\33[m')
+a = 3
+b = 5
+print('Os valores são \33[32m{}\33[m e \33[31m{}\33[m'.format(a, b))
---
+++ aula11b.py
@@ -0,0 +1,6 @@
+nome = 'Bezerra'
+cores = {'limpa':'\33[m]',
+         'azul':'\33[34m',
+         'amarelo':'\33[33m',
+         'pretoebranco':'\33[7;30m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
