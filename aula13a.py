a857e5a9a36cc920c417f89895e19eeeea0997de
Lucas <lucasbezerra06@gmail.com>
2018-06-01 09:29:16 -0300

    aula 13

---
+++ aula13a.py
@@ -0,0 +1,10 @@
+for c in range(1, 6):#não considera o ultimo numero, de 1 a 5
+    print(c)
+print('-='*10)
+for c in range(6, 0, -1):#range(inicio, fim, qual é a iteração, o que vai acontecer no finald do laço)
+    print(c)
+print('-='*10)
+for c in range(0, 7, 2):
+    print(c)
+print('-='*10)
+print('FIM')
